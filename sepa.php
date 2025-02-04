874053e890ef93a1b6b0419db819d8b5863a6941
Artur <kontakt@iprosoft.pl>
2025-02-04 20:02:57 +0100

    Civix Upgrade v23.02.0 => v23.02.1

--- sepa.php
+++ sepa.php
@@ -150,38 +150,10 @@ function sepa_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function sepa_civicrm_uninstall() {
-  //should we delete the tables?
-  return;
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function sepa_civicrm_enable() {
   return _sepa_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function sepa_civicrm_disable() {
-  return;
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function sepa_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return;
 }
 
 function sepa_civicrm_summaryActions( &$actions, $contactID ) {
@@ -592,14 +564,6 @@ function sepa_civicrm_tabset($tabsetName, &$tabs, $context) {
       'weight' => 20,
     ];
   }
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function sepa_civicrm_postInstall() {
 }
 
 // /**
